c6a6b3f98ea2ebb41d04ff0cbc10e2fa188d7a81
Robert Krahn <robert.krahn@gmail.com>
2014-07-31 01:33:14 -0700

    whoops, nrepl needed of course

--- examples/nrepl-server/project.clj
+++ examples/nrepl-server/project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cider/cider-nrepl "0.7.0-SNAPSHOT"]]
+                 [org.clojure/tools.nrepl "0.2.3"]]
   :main lively-connect.core)
