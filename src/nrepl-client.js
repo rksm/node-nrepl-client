09a7fc14609dcb270fb5984bac6e16c4fb3a27ce
Robert Krahn <robert.krahn@gmail.com>
2014-09-19 23:21:25 -0700

    adding custom con.send function to send arbitrary nREPL messages

--- src/nrepl-client.js
+++ src/nrepl-client.js
@@ -90,51 +90,61 @@ function nreplSend(socket, messageStream, msgSpec, callback) {
 
 // default nREPL ops, see https://github.com/clojure/tools.nrepl/blob/master/doc/ops.md
 
-function clone(socket, messageStream, session, callback) {
-    return nreplSend(socket, messageStream, {op: 'clone', session: session}, function(err, messages) {
+function clone(connection, session, callback) {
+    if (typeof session === 'function') { callback = session; session = undefined; }
+    return connection.send({op: 'clone', session: session}, function(err, messages) {
         var newSess = messages && messages[0] && messages[0]["new-session"];
-        if (newSess) socket.sessions.push(newSess);
+        if (newSess) connection.sessions.push(newSess);
         callback(err, messages);
     });
 }
 
-function close(socket, messageStream, session, callback) {
-    return nreplSend(socket, messageStream, {op: 'close', session: session}, function(err, messages) {
+function close(connection, session, callback) {
+    if (typeof session === 'function') { callback = session; session = undefined; }
+    return connection.send({op: 'close', session: session}, function(err, messages) {
         var status = messages && messages[0] && messages[0].status;
         var closed = status && status.indexOf("session-closed") > -1;
-        if (closed) socket.sessions = socket.sessions.filter(function(ea) { return ea != session; });
+        if (closed) connection.sessions = connection.sessions.filter(function(ea) { return ea != session; });
         callback(err, messages);
     });
 }
 
-function describe(socket, messageStream, verbose, callback) {
-    return nreplSend(socket, messageStream, {op: 'describe', 'verbose?': verbose ? 'true' : undefined}, callback);
+function describe(connection, verbose, callback) {
+    if (typeof verbose === 'function') { callback = verbose; verbose = undefined; }
+    return connection.send({op: 'describe', 'verbose?': verbose ? 'true' : undefined}, callback);
 }
 
-function cljEval(socket, messageStream, session, code, id, evalFunc, callback) {
-    return nreplSend(socket, messageStream, {op: 'eval', code: code, session: session, id: id, "eval": evalFunc}, callback);
+function cljEval(connection, code, session, id, evalFunc, callback) {
+    if (typeof session === 'function') { callback = session; session = undefined; }
+    else if (typeof id === 'function') { callback = id; id = undefined; }
+    else if (typeof evalFunc === 'function') { callback = evalFunc; evalFunc = undefined; }
+    return connection.send({op: 'eval', code: code, session: session, id: id, "eval": evalFunc}, callback);
 }
 
-function interrupt(socket, messageStream, session, id, callback) {
-    return nreplSend(socket, messageStream, {op: 'interrupt', "interrupt-id": id, session: session}, callback);
+function interrupt(connection, session, id, callback) {
+    if (typeof session === 'function') { callback = session; session = undefined; }
+    else if (typeof id === 'function') { callback = id; id = undefined; }
+    return connection.send({op: 'interrupt', "interrupt-id": id, session: session}, callback);
 }
 
-function loadFile(socket, messageStream, fileContent, fileName, filePath, callback) {
+function loadFile(connection, fileContent, fileName, filePath, callback) {
+    if (typeof fileName === 'function') { callback = fileName; fileName = undefined; }
+    else if (typeof filePath === 'function') { callback = filePath; filePath = undefined; }
     // :file-name Name of source file, e.g. io.clj
     // :file-path Source-path-relative path of the source file, e.g. clojure/java/io.clj
-    return nreplSend(socket, messageStream, {op: 'load-file', "file": fileContent, "file-name": fileName, "file-path": filePath}, callback);
+    return connection.send({op: 'load-file', "file": fileContent, "file-name": fileName, "file-path": filePath}, callback);
 }
 
-function lsSessions(socket, messageStream, callback) {
-    return nreplSend(socket, messageStream, {op: 'ls-sessions'}, function(err, messages) {
+function lsSessions(connection, callback) {
+    return connection.send({op: 'ls-sessions'}, function(err, messages) {
         var sessions = messages && messages[0] && messages[0]["sessions"];
-        if (sessions) socket.sessions = sessions;
+        if (sessions) connection.sessions = sessions;
         callback(err, messages);
     });
 }
 
-function stdin(socket, messageStream, stdin, callback) {
-    return nreplSend(socket, messageStream, {op: 'stdin', stdin: stdin}, callback);
+function stdin(connection, stdin, callback) {
+    return connection.send({op: 'stdin', stdin: stdin}, callback);
 }
 
 function connect(options) {
@@ -144,15 +154,16 @@ function connect(options) {
         messageStream.on("data", function(message) { console.log("nREPL message: ", message); });
     }
     con.sessions      = [];
-    con.clone         = clone.bind(null, con, messageStream);
-    con.close         = close.bind(null, con, messageStream);
-    con.describe      = describe.bind(null, con, messageStream);
-    con.eval          = cljEval.bind(null, con, messageStream);
-    con.interrupt     = interrupt.bind(null, con, messageStream);
-    con.loadFile      = loadFile.bind(null, con, messageStream);
-    con.lsSessions    = lsSessions.bind(null, con, messageStream);
-    con.stdin         = stdin.bind(null, con, messageStream);
     con.messageStream = messageStream;
+    con.send          = nreplSend.bind(null, con, messageStream);
+    con.clone         = clone.bind(null, con);
+    con.close         = close.bind(null, con);
+    con.describe      = describe.bind(null, con);
+    con.eval          = cljEval.bind(null, con);
+    con.interrupt     = interrupt.bind(null, con);
+    con.loadFile      = loadFile.bind(null, con);
+    con.lsSessions    = lsSessions.bind(null, con);
+    con.stdin         = stdin.bind(null, con);
     return con;
 }
 
