8aa35dc5942b491f95cda8efbadd76c58d567869
Robert Krahn <robert.krahn@gmail.com>
2014-09-19 22:34:03 -0700

    adding nrepl handlers for clone, close, interrupt, loadFile, lsSessions, stdin

--- src/nrepl-client.js
+++ src/nrepl-client.js
@@ -75,18 +75,53 @@ function nreplSend(socket, messageStream, msg, callback) {
     });
 }
 
-function cljEval(socket, messageStream, code, callback) {
-    nreplSend(socket, messageStream, {op: 'eval', "code": code}, function(err, result) {
-        callback && callback(err,
-            result && result.length && result[0].value ?
-                result[0].value : null);
+// default nREPL ops, see https://github.com/clojure/tools.nrepl/blob/master/doc/ops.md
+
+function clone(socket, messageStream, session, callback) {
+    return nreplSend(socket, messageStream, {op: 'clone', session: session}, function(err, messages) {
+        var newSess = messages && messages[0] && messages[0]["new-session"];
+        if (newSess) socket.sessions.push(newSess);
+        callback(err, messages);
+    });
+}
+
+function close(socket, messageStream, session, callback) {
+    return nreplSend(socket, messageStream, {op: 'close', session: session}, function(err, messages) {
+        var status = messages && messages[0] && messages[0].status;
+        var closed = status && status.indexOf("session-closed") > -1;
+        if (closed) socket.sessions = socket.sessions.filter(function(ea) { return ea != session; });
+        callback(err, messages);
     });
 }
 
 function describe(socket, messageStream, verbose, callback) {
-    nreplSend(socket, messageStream, {op: 'describe', 'verbose?': verbose ? 'true' : undefined}, function(err, result) {
-        callback && callback(err, result && result.length ? result[0] : null);
+    return nreplSend(socket, messageStream, {op: 'describe', 'verbose?': verbose ? 'true' : undefined}, callback);
+}
+
+function cljEval(socket, messageStream, session, code, id, evalFunc, callback) {
+    return nreplSend(socket, messageStream, {op: 'eval', code: code, session: session, id: id, "eval": evalFunc}, callback);
+}
+
+function interrupt(socket, messageStream, session, id, callback) {
+    return nreplSend(socket, messageStream, {op: 'interrupt', "interrupt-id": id, session: session}, callback);
+}
+
+function loadFile(socket, messageStream, fileContent, fileName, filePath, callback) {
+    // :file-name Name of source file, e.g. io.clj
+    // :file-path Source-path-relative path of the source file, e.g. clojure/java/io.clj
+    return nreplSend(socket, messageStream, {op: 'load-file', "file": fileContent, "file-name": fileName, "file-path": filePath}, callback);
+}
+
+function lsSessions(socket, messageStream, callback) {
+    return nreplSend(socket, messageStream, {op: 'ls-sessions'}, function(err, messages) {
+        var sessions = messages && messages[0] && messages[0]["sessions"];
+        if (sessions) socket.sessions = sessions;
+        callback(err, messages);
     });
+}
+
+function stdin(socket, messageStream, stdin, callback) {
+    return nreplSend(socket, messageStream, {op: 'stdin', stdin: stdin}, callback);
 }
 
 function connect(options) {
@@ -95,8 +130,15 @@ function connect(options) {
     if (options.verbose) {
         messageStream.on("data", function(message) { console.log("nREPL message: ", message); });
     }
-    con.eval = cljEval.bind(null, con, messageStream);
-    con.describe = describe.bind(null, con, messageStream);
+    con.sessions      = [];
+    con.clone         = clone.bind(null, con, messageStream);
+    con.close         = close.bind(null, con, messageStream);
+    con.describe      = describe.bind(null, con, messageStream);
+    con.eval          = cljEval.bind(null, con, messageStream);
+    con.interrupt     = interrupt.bind(null, con, messageStream);
+    con.loadFile      = loadFile.bind(null, con, messageStream);
+    con.lsSessions    = lsSessions.bind(null, con, messageStream);
+    con.stdin         = stdin.bind(null, con, messageStream);
     con.messageStream = messageStream;
     return con;
 }
